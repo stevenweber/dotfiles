b060efe39a983eac66abdd3f739fa2e760327bd3
Steve Weber <sweber@groupon.com>
2015-05-19 10:43:16 -0500

    only initialize boot2docker ip if it's running

--- etc/boot2docker.bash
+++ etc/boot2docker.bash
@@ -1,3 +1,6 @@
 if [ `which boot2docker` ]; then
-  $(boot2docker shellinit 2> /dev/null)
+  if [ "$(boot2docker status)" == "running" ]; then
+    $(boot2docker shellinit 2> /dev/null)
+    export DOCKER_IP=$(boot2docker ip)
+  fi
 fi
