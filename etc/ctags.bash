c5b678ddf205289553b3d9e9092ddc131bbabf3f
Steve Weber <steve@copyright1984.com>
2018-11-04 12:25:24 -0600

    adding ctag support for js
    ref: https://medium.com/adorableio/modern-javascript-ctags-configuration-199884dbcc1

--- etc/ctags.bash
+++ etc/ctags.bash
@@ -1,3 +1,7 @@
 function tags() {
   ctags -f .tags -L $(git-files)
 }
+
+function jtags() {
+  ctags -R src && sed -i '' -E '/^(if|switch|function|module\.exports|it|describe).+language:js$/d' tags
+}
