602f2fa776b2700890c5bd20f170d45ec6afc6ac
Steve Weber <steve@copyright1984.com>
2018-11-04 10:40:12 -0600

    update prompt

--- etc/vcprompt.bash
+++ etc/vcprompt.bash
@@ -1,5 +1,5 @@
 if [ `which vcprompt` ]; then
   # http://hg.gerg.ca/vcprompt
-  export VCPROMPT_FORMAT="%b%m%u "
-  export PS1="\[$Cyan\]\h\[$Color_Off\]:\w \[$Green\]\$(vcprompt)\[$Color_Off\]\u$ "
+  export VCPROMPT_FORMAT="%b%m "
+  export PS1="\[$Cyan\]\h\[$Color_Off\]:\w \[$Green\]\$(vcprompt)\[$Color_Off\]\u $ "
 fi
