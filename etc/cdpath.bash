e540e090e7e7aa371afc6cc21d397020e7577db9
Steve Weber <steve@copyright1984.com>
2018-11-04 10:27:28 -0600

    cdpath, vimrc and vim bundle updates

--- etc/cdpath.bash
+++ etc/cdpath.bash
@@ -1,3 +1,7 @@
 if [ -a $HOME/projects ]; then
-  export CDPATH=".:$HOME/projects/:$HOME/"
+  export CDPATH=".:$HOME/projects/:$HOME/:$CDPATH"
+fi
+
+if [ -a $GOPATH ]; then
+  export CDPATH="$CDPATH:$GOPATH/src/:$GOPATH/src/github.com/"
 fi
