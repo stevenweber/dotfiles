bb52aded2b6e2dfaff7ba6695baaf165b2b90821
Steve Weber <steve@copyright1984.com>
2018-11-04 13:43:54 -0600

    moving ctag helpers to bin

--- etc/ctags.bash
+++
@@ -1,7 +0,0 @@
-function tags() {
-  ctags -f .tags -L $(git-files)
-}
-
-function jtags() {
-  ctags -R $1 && sed -i '' -E '/^(if|switch|function|module\.exports|it|describe).+language:js$/d' tags
-}
