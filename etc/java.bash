3cffa7885645bdd4310fb95c736dfbf6118fd987
Steve Weber <stevew@reverb.com>
2018-11-19 12:03:35 -0600

    don't complain about not having java installed

--- etc/java.bash
+++ etc/java.bash
@@ -2,6 +2,4 @@
 if [ $? -eq 0 ]
 then
   export JAVA_HOME=$(/usr/libexec/java_home)
-else
-  echo "[warning] no java installed, http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html"
 fi
