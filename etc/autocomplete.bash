3daea2d82dd5f7414df6f6a23ca5fd8651a55a8b
Steve Weber <steve@copyright1984.com>
2019-04-07 09:29:52 -0500

    use bash 5

--- etc/autocomplete.bash
+++ etc/autocomplete.bash
@@ -1,13 +1,5 @@
 bind 'set completion-ignore-case on'
 
-if [ -f $(brew --prefix)/etc/bash_completion ]; then
-  . $(brew --prefix)/etc/bash_completion
-fi
-
-if [ -f `brew --prefix`/etc/bash_completion.d/git-completion.bash ]; then
-  . `brew --prefix`/etc/bash_completion.d/git-completion.bash
-fi
-
-if [ -f `brew --prefix`/etc/bash_completion.d/git-prompt.sh ]; then
-  . `brew --prefix`/etc/bash_completion.d/git-prompt.sh
+if [[ -r "$(brew --prefix)/etc/profile.d/bash_completion.sh" ]]; then
+  . "$(brew --prefix)/etc/profile.d/bash_completion.sh"
 fi
