289785ebf011ad71947949c6065ed2b1f94d8011
Steve Weber <stevew@reverb.com>
2019-04-08 13:45:42 -0500

    add git tweaks

--- etc/autocomplete.bash
+++ etc/autocomplete.bash
@@ -3,3 +3,11 @@ bind 'set completion-ignore-case on'
 if [[ -r "$(brew --prefix)/etc/profile.d/bash_completion.sh" ]]; then
   . "$(brew --prefix)/etc/profile.d/bash_completion.sh"
 fi
+
+if [ -f `brew --prefix`/etc/bash_completion.d/git-completion.bash ]; then
+  . `brew --prefix`/etc/bash_completion.d/git-completion.bash
+fi
+
+if [ -f `brew --prefix`/etc/bash_completion.d/git-prompt.sh ]; then
+  . `brew --prefix`/etc/bash_completion.d/git-prompt.sh
+fi
