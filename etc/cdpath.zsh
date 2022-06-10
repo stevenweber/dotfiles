25c4beac109a85c5014a630c65e7df8ab313dc22
Steve Weber <steve@copyright1984.com>
2022-06-10 10:29:30 -0500

    pyenv and general updates

--- etc/cdpath.zsh
+++ etc/cdpath.zsh
@@ -1,5 +1,9 @@
 setopt auto_cd
 
 if [ -d ${HOME}/projects ]; then
-  cdpath=(${HOME} ${HOME}/projects)
+  cdpath+=(${HOME} ${HOME}/projects)
+fi
+
+if [ -d ${HOME}/Documents/reverb ]; then
+  cdpath+=(${HOME} ${HOME}/Documents/reverb)
 fi
