d6e8887f219a232ea59f3a4eb8dcf6826a1d4356
Steve <stevew@reverb.com>
2026-04-23 10:11:55 -0500

    moving to asdf

--- etc/path.zsh
+++ etc/path.zsh
@@ -1,4 +1,4 @@
 export GOPATH="$HOME/projects/go"
 export VISUAL_STUDIO_CODE_APP="$HOME/Applications/Visual Studio Code.app/Contents/Resources/app"
 
-export PATH="/opt/homebrew/bin:$GOPATH/bin:$PATH:$DOTFILES/bin:${VISUAL_STUDIO_CODE_APP}/bin:$HOME/bin:"
+export PATH="$HOME/.local/bin:/opt/homebrew/bin:$GOPATH/bin:$PATH:$DOTFILES/bin:${VISUAL_STUDIO_CODE_APP}/bin:$HOME/bin:"
